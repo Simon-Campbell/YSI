8a2626d1d6e2c33f8dd7e67adefe194bc65c94ac
Simon Campbell <simon.s.campbell@gmail.com>
2012-11-02 18:22:43 +1300

    Initial work on the document converter. Defined a conversion rules file and started writing the script.

---
+++ devscripts/doc-converter.py
@@ -0,0 +1,18 @@
+def make_intermediate(text):
+	# convert text to an intermediate form
+	
+def convert_function(intermediate):
+	# convert an intermediate to a string representation
+	# which can be dumped to a file
+
+class YsiParser( object ):
+	# parse a YSI function header
+	
+	
+class FunctionDescriptor( object ):
+	# describe a function
+	summary = ''
+	returns	= ''
+	params	= []
+	notes	= ''
+	

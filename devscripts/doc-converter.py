69bed726ccf8eacfb84a9df9ed463e9eeffaa9b4
Simon Campbell <simon.s.campbell@gmail.com>
2012-11-02 22:10:27 +1300

    Basic parsing of the test file. Just prints out signals at this stage.
    Signed-off-by: Simon Campbell <simon.s.campbell@gmail.com>

--- devscripts/doc-converter.py
+++ devscripts/doc-converter.py
@@ -1,18 +1,105 @@
+import io
+
+filepos = 0
+
+def main():
+    # open a test file for reading
+    with io.open('test.pwn') as file:
+                filepos = 0
+                line = file.readline()
+
+                while (line != ""):
+                        if (is_open_doc_comment(line)):                              
+                                parse_comment(file)
+                                
+                        line = file.readline()
+
+def parse_comment(file):
+        line = file.readline()
+
+        while (line != ""):
+                if (is_close_doc_comment(line)):
+                        break
+                elif (is_doc_summary(line)):
+                        print get_doc_summary(file)
+
+                line = file.readline()
+
+def is_doc_summary(text):
+        stripped = text.lstrip()
+
+        if (stripped.startswith('Function:')):
+                print 'doc-converter: Found YSI comment summary'
+
+                return True
+        else:
+                return False
+
+def get_doc_summary(file):
+        line = file.readline()
+        summary = ''
+
+        while (line != ""):
+                if (is_end_doc_section(line)):
+                        break
+                else:
+                        summary += line
+
+                line = file.readline()
+                
+        return summary
+
+def is_end_doc_section(text):
+        return is_close_doc_comment(text)
+                        
+def is_open_doc_comment(text):
+        stripped = text.lstrip()
+        
+        if (stripped.startswith('/*----------------------------------------------------------------------------*\\')):
+                print 'doc-converter: Found YSI comment opening'
+                
+                return True
+        else:
+                return False
+
+def is_close_doc_comment(text):
+        stripped = text.lstrip()
+        
+        if (stripped.startswith('/*----------------------------------------------------------------------------*\\')):
+                print 'doc-converter: Found YSI comment closing'
+                
+                return True
+        else:
+                return False
+        
+        
 def make_intermediate(text):
-	# convert text to an intermediate form
-	
+    # convert text to an intermediate form
+    pass
+    
 def convert_function(intermediate):
-	# convert an intermediate to a string representation
-	# which can be dumped to a file
+    # Convert an intermediate to a string representation
+    # which can be dumped to a file
+    pass
 
 class YsiParser( object ):
-	# parse a YSI function header
-	
-	
+    # Parse a YSI function header to
+    # a FunctionDescriptor
+    pass
+    
+# Generate PawnDocs using a FunctionDescriptor
+# object
+class PawnDocGenerator( object ):   
+    def get_as_string():
+        pawndoc = ''
+    
 class FunctionDescriptor( object ):
-	# describe a function
-	summary = ''
-	returns	= ''
-	params	= []
-	notes	= ''
-	
+    # Describe a function
+    summary = ''
+    returns = ''
+    params  = []
+    notes   = ''
+    
+if __name__ == "__main__":
+    main()
+

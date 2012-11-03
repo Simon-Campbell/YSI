55410d7cc86d8fe57cda88ad737418f7e39456bd
Simon Campbell <simon.s.campbell@gmail.com>
2012-11-03 14:05:38 +1300

    Full parsing of YSI doc into a data structure (using test.pwn). Needs testing on proper source files.

--- devscripts/doc-converter.py
+++ devscripts/doc-converter.py
@@ -9,8 +9,9 @@ def main():
                 line = file.readline()
 
                 while (line != ""):
-                        if (is_open_doc_comment(line)):                              
-                                parse_comment(file)
+                        if (is_open_doc_comment(line)):
+                                print 'YSI Doc:'
+                                print parse_comment(file)
                                 
                         line = file.readline()
 
@@ -19,20 +20,30 @@ def parse_comment(file):
         description = {}
 
         while (line != ""):
-                if (is_close_doc_comment(line)):
-                    break
-                elif (is_doc_summary(line)):
-                       description['summary'], line = get_doc_summary(file)
-                elif (is_doc_params(line)):
-                        description['params'], line = get_doc_params(file)
-                else:
+                if is_close_doc_comment(line):
                     line = file.readline()
-
-        print description
-
+                    break
+                elif is_doc_summary(line):
+                       description['summary'], line = get_doc_summary(file)
+                elif is_doc_params(line):
+                        description['params'], line = get_doc_params(file)
+                elif is_note_doc_header(line):
+                        description['note'], line = get_doc_note(file)
+                elif is_return_doc_header(line):
+                        description['return'], line = get_doc_return(file)
+                else:
+                    # unknown doc header/content consume
+                    line = file.readline()
+                    
+                    print 'doc-converter: ' + line
+                    print 'doc-converter: Unknown document header found in comment context'
+                    print 'doc-converter: the line has been skipped'
+                    
+        return description
+    
 def is_doc_summary(text):
         if is_doc_header('Function', text):
-            print 'doc-converter: Found YSI comment summary'
+            # print 'doc-converter: Found YSI comment summary'
 
             return True
         else:
@@ -40,7 +51,7 @@ def is_doc_summary(text):
 
 def is_doc_params(text):
     if is_doc_header('Params', text):
-        print 'doc-converter: Found YSI parameter list'
+        # print 'doc-converter: Found YSI parameter list'
         
         return True
     else:
@@ -54,53 +65,89 @@ def is_doc_header(header, text):
     else:
         return False
 
-def get_doc_summary(file):
-        line = file.readline()
-        summary = ''
-
-        while (line != ""):
-                if (is_end_doc_section(line)):
-                        break
-                else:
-                        summary += line
-
-                line = file.readline()
-                
-        return summary, line
-
 def get_doc_params(file):
         line = file.readline()
         params = []
         last_param = {}
-        is_continued = False
         
         while (line != ""):
             if (is_end_doc_section(line)):
                 break
             else:
-                # param <= name - documentation
+                # Param:
+                #   (Name) - (Documentation)
+                #   ([A-za-z_][A-Za-z0-9_]+) - (.*)
                 param = line.split('-', 1)
 
                 if len(param) < 2:
-                    print 'continue'
+                    # fppend to the last parameters
+                    # documentation
+                    last_param[last_param.keys()[0]] += param[0].lstrip()
                 else:
-                    print 'got param'
-                    last_param = { param[0].strip(): param[1].strip() }
-                    params.append()
+                    # Found a new parameter so set last param to
+                    # equal it and append it to the parameter list
+                    last_param = { param[0].strip(): param[1].lstrip() }
+                    params.append(last_param)
 
             line = file.readline()
 
         return params, line
 
+def get_doc_summary(file):
+    return get_doc_text(file)
+
+def get_doc_note(file):
+    return get_doc_text(file)
+
+def get_doc_return(file):
+    return get_doc_text(file)
+
+# general method to get chunk of text given that
+# header has just been seen. method breaks when
+# doc section ends
+def get_doc_text(file):
+    line = file.readline()
+    text = ''
+
+    while (line != ""):
+        if is_end_doc_section(line):
+            break
+        else:
+            # get rid of preceding tabs, keep right
+            # whitespace (e.g. new lines)
+            text += line.lstrip()
+        line = file.readline()
+    return text, line
+    
 
 def is_end_doc_section(text):
-        return is_close_doc_comment(text) or is_doc_params(text) or is_doc_summary(text)
-                        
+        return is_close_doc_comment(text) or \
+               is_doc_params(text) or \
+               is_doc_summary(text) or \
+               is_return_doc_header(text) or \
+               is_note_doc_header(text)
+
+def is_return_doc_header(text):
+    if is_doc_header('Return', text):
+        # print 'doc-converter: Found YSI return documentation'
+        
+        return True
+    else:
+        return False
+
+def is_note_doc_header(text):
+    if is_doc_header('Notes', text):
+        # print 'doc-converter: Found YSI note'
+        
+        return True
+    else:
+        return False
+    
 def is_open_doc_comment(text):
         stripped = text.lstrip()
         
         if (stripped.startswith('/*----------------------------------------------------------------------------*\\')):
-                print 'doc-converter: Found YSI comment opening'
+                # print 'doc-converter: Found YSI comment opening'
                 
                 return True
         else:
@@ -109,8 +156,8 @@ def is_open_doc_comment(text):
 def is_close_doc_comment(text):
         stripped = text.lstrip()
         
-        if (stripped.startswith('/*----------------------------------------------------------------------------*\\')):
-                print 'doc-converter: Found YSI comment closing'
+        if (stripped.startswith('\\*----------------------------------------------------------------------------*/')):
+                # print 'doc-converter: Found YSI comment closing'
                 
                 return True
         else:
--- devscripts/test.pwn
+++ devscripts/test.pwn
@@ -5,4 +5,20 @@
 		Name - This parameter does stuff!
 		AnotherParam - This one does something different and
 			spans multiple lines
-/*----------------------------------------------------------------------------*\
+	Return
+\*----------------------------------------------------------------------------*/
+
+
+/*----------------------------------------------------------------------------*\ 
+Function: 
+    A function summary
+Params: 
+    Name1 - The name of a certain parameter
+	Name2 - The name of a certain multi-line parameter
+			that spans multiple lines
+Return: 
+    This function returns some awesome stuff
+Notes: 
+    I would like to note that this function has some form of notes
+	I hope that these notes are sufficient
+\*----------------------------------------------------------------------------*/  
